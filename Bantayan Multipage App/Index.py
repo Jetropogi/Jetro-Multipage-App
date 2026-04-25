8789cf9254261be2077e1574b298a937a3a1ec2c
Jetro <jetroguardian@gmail.com>
2026-04-25 12:10:24 +0900

    Add page navigation to the portfolio app

--- Bantayan Multipage App/Index.py
+++ Bantayan Multipage App/Index.py
@@ -6,6 +6,19 @@ st.title("Welcome to my Portfolio")
 
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Dashboard", "About Me", "Skills", "Contacts"])
+
+if page == "Dashboard":
+    st.title("🏠 Dashboard")
+
+elif page == "About Me":
+    st.title("🙋 About Me")
+
+elif page == "Skills":
+    st.title("🎨 Skills")
+
+elif page == "Contacts":
+    st.title("📱 Contacts")
+
 st.write("""
 Navigate through my portfolio using the sidebar👌
 
